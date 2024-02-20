603626dabf50a6cb2aaa272a0e8fb13f6ac7cf21
Shirin <shirin.mohajer@gmail.com>
2024-02-20 10:09:20 -0500

    Time: 125 ms (9.83%), Space: 57.4 MB (11.65%) - LeetHub

--- 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.js
+++ 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.js
@@ -1,18 +1,19 @@
 var firstUniqChar = function(s) {
     let sArray = s.split("")
     
-    let sObj = {}
+    let sObj = {}//create an empty object to track the repetition of letters
 
     sArray.forEach(el => {
         if(sObj.hasOwnProperty(el)){
-            sObj[el]++
-        }else{
-            sObj[el] = 1
+            sObj[el]++ //if the key already exists in the object, increment at sObj[<<KEY>>]
+            
+        }else{//if the key does not exists create the key and set the value to 1
+            sObj[el] = 1 
         }
     } )
     console.log(sObj)
     let keys = Object.keys(sObj)
-    console.log(keys)
+    //console.log(keys)
     for(let i = 0; i<keys.length; i++){
         if(sObj[keys[i]] === 1){
             return sArray.indexOf(keys[i])
