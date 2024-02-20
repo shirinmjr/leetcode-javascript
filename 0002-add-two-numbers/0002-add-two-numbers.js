30ff81005c95342efe71227fbd1e2bb9e31234bb
Shirin <shirin.mohajer@gmail.com>
2024-02-20 17:02:31 -0500

    Time: 107 ms (46.99%), Space: 54.8 MB (52.29%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.js
+++ 0002-add-two-numbers/0002-add-two-numbers.js
@@ -25,15 +25,17 @@ var addTwoNumbers = function (l1, l2) {
             sum += l2.val
             l2 = l2.next;
         }
-        current.next = new ListNode(sum % 10)
         carry = Math.floor(sum / 10)
-
+        current.next = new ListNode(sum % 10)
         current = current.next;
+        
+ 
+    }
         if (carry > 0) {
             current.next = new ListNode(carry)
 
         }
-    }
-        return result.next;
+         return result.next;
+       
 
 };
