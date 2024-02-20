79a77edc1c407dd8421b1ee7637c2e0ddeebdbf1
Shirin <shirin.mohajer@gmail.com>
2024-02-20 12:40:32 -0500

    Time: 76 ms (40.81%), Space: 53.7 MB (6.40%) - LeetHub

---
+++ 0231-power-of-two/0231-power-of-two.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+var isPowerOfTwo = function (n) {
+   if (n <=0) return false
+    if (n === 1)
+        return true;
+    if (n % 2 === 0) {
+       return isPowerOfTwo(Number(n/2));
+    }
+    else {
+        return false;
+    }
+};
