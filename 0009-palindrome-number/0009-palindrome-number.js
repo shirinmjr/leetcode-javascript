5e719627cdb132f2b66dbbc611a867b1643c5c50
Shirin <shirin.mohajer@gmail.com>
2024-02-26 15:36:47 -0500

    Time: 119 ms (92.96%), Space: 57.3 MB (39.52%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.js
@@ -0,0 +1,9 @@
+/**
+ * @param {number} x
+ * @return {boolean}
+ */
+var isPalindrome = function (x) {
+    let xString = x.toString();//turning the number to a String
+    let xReverse = xString.split('').reverse().join('');//String => Array => reverse => join back as String
+    return xString === xReverse //compaire two String
+}
