9845b1dadbc0f4957dfd669953090438ea31183d
Shirin <shirin.mohajer@gmail.com>
2024-04-15 15:54:33 -0400

    Time: 54 ms (58.32%), Space: 49 MB (48.26%) - LeetHub

--- 1492-the-kth-factor-of-n/1492-the-kth-factor-of-n.js
+++ 1492-the-kth-factor-of-n/1492-the-kth-factor-of-n.js
@@ -5,13 +5,8 @@
  */
 var kthFactor = function (n, k) {
   const factors = [];
-  let i = 1;
-  while (i <= n) {
-    if (n % i === 0) {
-      factors.push(i);
-    }
-    i++;
+for (let i=1; i<= n;i++) {
+    if (n % i === 0) factors.push(i);
   }
-  console.log(factors);
   return factors[k - 1] ? factors[k - 1] : -1;
 };
