7565cb3435d003cf758db7eceed07a11556d3d14
Shirin <shirin.mohajer@gmail.com>
2024-04-17 14:27:28 -0400

    Time: 53 ms (53.26%), Space: 50.9 MB (5.17%) - LeetHub

--- 1512-number-of-good-pairs/1512-number-of-good-pairs.js
+++ 1512-number-of-good-pairs/1512-number-of-good-pairs.js
@@ -3,13 +3,23 @@
  * @return {number}
  */
 var numIdenticalPairs = function (nums) {
-    let result = 0;
-    for (let i = 0; i <= nums.length; i++) {
-        for (let j = 0; j <= nums.length; j++) {
-            if (nums[i] === nums[j] && i != j)
-                result++
-        }
-    }
-
-    return result / 2
+    let count = 0
+    nums.forEach((num, index) => {
+        let searchArray = nums.slice((index + 1))
+        let result = searchArray.filter(item => item === num)
+        count += result.length
+    })
+    return count
 };
+
+// var numIdenticalPairs = function (nums) {
+//     let result = 0;
+//     for (let i = 0; i <= nums.length; i++) {
+//         for (let j = 0; j <= nums.length; j++) {
+//             if (nums[i] === nums[j] && i != j)
+//                 result++
+//         }
+//     }
+
+//     return result / 2
+// };
