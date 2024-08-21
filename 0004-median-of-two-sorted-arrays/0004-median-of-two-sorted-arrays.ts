197287abf2e7952c2871bc713618f1d3d5a99b69
Shirin <shirin.mohajer@gmail.com>
2024-08-21 15:25:47 -0400

    Time: 126 ms (13.98%), Space: 57 MB (32.47%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.ts
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.ts
@@ -1,25 +1,16 @@
 function findMedianSortedArrays(nums1: number[], nums2: number[]): number {
     let nums: number[] = [];
-    let nums1L = nums1.length;
-    let nums2L = nums2.length;
 
-    const merge = (arr1: number[], arr2: number[]): number[] => {
-        while (arr1.length && arr2.length) {
-            if (arr1[0] < arr2[0]) {
-                nums.push(arr1.shift());
+        while (nums1.length && nums2.length) {
+            if (nums1[0] < nums2[0]) {
+                nums.push(nums1.shift());
             } else {
-                nums.push(arr2.shift());
+                nums.push(nums2.shift());
             }
         }
+        nums = [...nums, ...nums1, ...nums2];
 
-        nums = [...nums, ...arr1, ...arr2];
-        return nums;
-    }
-
-    merge(nums1, nums2);
-    
     if (nums.length % 2 == 1) return nums[Math.floor(nums.length / 2)];
 
-    else return (nums[(nums.length / 2) - 1] + nums[(nums.length / 2)]) / 2;
-    
+    else return (nums[(nums.length / 2) - 1] + nums[(nums.length / 2)]) / 2;   
 };
