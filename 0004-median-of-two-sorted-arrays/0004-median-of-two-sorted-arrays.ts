a9f9347a01045fccb7ac60149ca970927b6ebd31
Shirin <shirin.mohajer@gmail.com>
2024-08-21 15:31:40 -0400

    Time: 98 ms (67.69%), Space: 57.7 MB (14.88%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.ts
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.ts
@@ -1,14 +1,14 @@
 function findMedianSortedArrays(nums1: number[], nums2: number[]): number {
     let nums: number[] = [];
 
-        while (nums1.length && nums2.length) {
-            if (nums1[0] < nums2[0]) {
+        while (nums1.length && nums2.length) {//to combine two arrays
+            if (nums1[0] < nums2[0]) { 
                 nums.push(nums1.shift());
             } else {
-                nums.push(nums2.shift());
+                nums.push(nums2.shift()); 
             }
         }
-        nums = [...nums, ...nums1, ...nums2];
+        nums = [...nums, ...nums1, ...nums2];//combined array + what is left from the longer array
 
     if (nums.length % 2 == 1) return nums[Math.floor(nums.length / 2)];
 
