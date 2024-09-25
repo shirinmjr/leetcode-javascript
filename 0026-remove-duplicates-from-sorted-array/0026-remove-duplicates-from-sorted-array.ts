fa2b92342dfbe13ecb2add7c2e75d67dc49df871
Shirin <shirin.mohajer@gmail.com>
2024-09-25 16:30:21 -0400

    Time: 74 ms (38.29%), Space: 53.7 MB (15.54%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.ts
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.ts
@@ -3,16 +3,16 @@ function removeDuplicates(nums: number[]): number {
 
     let uniqueIndex = 1;
 
-    for (let i = 0; i < nums.length -1; i++) {
+    for (let i = 0; i < nums.length - 1; i++) {
         // Only move forward if we find a new unique number
-        if (nums[i] !== nums[i+1]) {
-          
-            nums[uniqueIndex] = nums[i+1];
-              uniqueIndex++;
-            
+        if (nums[i] !== nums[i + 1]) {
+
+            nums[uniqueIndex] = nums[i + 1];
+            uniqueIndex++;
+
         }
     }
 
     // The number of unique elements will be uniqueIndex + 1
-    return uniqueIndex ;
+    return uniqueIndex;
 };
