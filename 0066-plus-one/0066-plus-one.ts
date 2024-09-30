14622c9f707c5759dd66ce8042c8138b95aea1f1
Shirin <shirin.mohajer@gmail.com>
2024-09-30 19:30:11 -0400

    Time: 57 ms (58.51%), Space: 51.6 MB (12.63%) - LeetHub

--- 0066-plus-one/0066-plus-one.ts
+++ 0066-plus-one/0066-plus-one.ts
@@ -1,7 +1,7 @@
 function plusOne(digits: number[]): number[] {
 
     let str = digits.join("")
-    let increment = BigInt(str) + BigInt(1)
+    let increment = BigInt(str) + BigInt(1)//To handle large integers
     let strArray = increment.toString().split("")
     return strArray.map(str => Number(str))
 
