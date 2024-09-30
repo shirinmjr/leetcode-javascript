b041fc0fda959fe69d08a4ebca093bca1de30621
Shirin <shirin.mohajer@gmail.com>
2024-09-30 19:54:25 -0400

    Time: 59 ms (42.17%), Space: 51.5 MB (6.84%) - LeetHub

---
+++ 2813-to-be-or-not-to-be/2813-to-be-or-not-to-be.ts
@@ -0,0 +1,25 @@
+type ToBeOrNotToBe = {
+    toBe: (val: any) => boolean;
+    notToBe: (val: any) => boolean;
+};
+
+function expect(val: any): ToBeOrNotToBe {
+    return {
+        toBe: function (val2: any) {
+            if (val === val2) {
+                return true
+            } else { throw new Error("Not Equal") }
+        },
+        notToBe: function (val2: any) {
+            if (val !== val2) {
+                return true
+            }
+            else { throw new Error("Equal") }
+        }
+    }
+};
+
+/**
+ * expect(5).toBe(5); // true
+ * expect(5).notToBe(5); // throws "Equal"
+ */
