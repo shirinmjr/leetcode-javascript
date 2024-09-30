e58cfa112c73126c5fbc152ada7902df1389a366
Shirin <shirin.mohajer@gmail.com>
2024-09-30 14:04:08 -0400

    Time: 59 ms (46.75%), Space: 52.1 MB (7.03%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.ts
+++ 0035-search-insert-position/0035-search-insert-position.ts
@@ -1,19 +1,13 @@
 function searchInsert(nums: number[], target: number): number {
-
     for (let i = 0; i < nums.length; i++) {
     
         if (nums[i] === target) {
             return i
-       
         } else if (nums[i] < target && target < nums[i + 1]) {
-          
             return i + 1
-        }else if ( target < nums[0] && i===0){
-             
+        }else if ( target < nums[0] && i===0){     
            return 0
         }
-
     }
     return nums.length
-
 };
