9140bba1bb3b562078d04789cdc29129fdf13a26
Shirin <shirin.mohajer@gmail.com>
2024-09-30 20:38:16 -0400

    Time: 58 ms (47.46%), Space: 51.6 MB (10.28%) - LeetHub

--- 2747-apply-transform-over-each-element-in-array/2747-apply-transform-over-each-element-in-array.ts
+++ 2747-apply-transform-over-each-element-in-array/2747-apply-transform-over-each-element-in-array.ts
@@ -1,8 +1,3 @@
 function map(arr: number[], fn: (n: number, i: number) => number): number[] {
-    let result = arr.map((n,i) => {
-        return fn(arr[i], i)
-
-    })
-
-    return result
+    return arr.map((n,i) =>  fn(arr[i], i))
 };
