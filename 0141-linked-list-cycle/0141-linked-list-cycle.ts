aab4652ca64d62cc448658f2aad3500c11e42635
Shirin <shirin.mohajer@gmail.com>
2024-10-21 09:27:30 -0400

    Time: 79 ms (17.91%), Space: 54.1 MB (45.44%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.ts
+++ 0141-linked-list-cycle/0141-linked-list-cycle.ts
@@ -16,7 +16,7 @@ function hasCycle(head: ListNode | null): boolean {
     while (fast !== null && fast.next !== null) {
         slow = slow.next;
         fast = fast.next.next;
-        if (fast === slow) return true
+        if (fast === slow) return true;
     }
-    return false
+    return false;
 };
