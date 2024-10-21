8b38b32ac6a40a38a8998346fe264b512944ef20
Shirin <shirin.mohajer@gmail.com>
2024-10-21 09:27:18 -0400

    Time: 68 ms (64.23%), Space: 53.2 MB (89.89%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.ts
+++ 0141-linked-list-cycle/0141-linked-list-cycle.ts
@@ -11,12 +11,12 @@
  */
 
 function hasCycle(head: ListNode | null): boolean {
-let slow = head
-let fast = head
-while(fast!==null &&fast.next !==null){
-slow=slow.next
-fast=fast.next.next
-if(fast===slow)return true
-}
-return false
+    let slow = head;
+    let fast = head;
+    while (fast !== null && fast.next !== null) {
+        slow = slow.next;
+        fast = fast.next.next;
+        if (fast === slow) return true
+    }
+    return false
 };
