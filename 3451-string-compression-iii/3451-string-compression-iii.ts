d4413487c7e83cc0df445f6b82a8fe227f546b84
Shirin <shirin.mohajer@gmail.com>
2024-11-04 15:37:39 -0500

    Time: 27 ms (100%), Space: 64 MB (100%) - LeetHub

--- 3451-string-compression-iii/3451-string-compression-iii.ts
+++ 3451-string-compression-iii/3451-string-compression-iii.ts
@@ -3,16 +3,16 @@ function compressedString(word: string): string {
     let n: number = word.length;
 
 
-    for (let i: number = 0; i < n; ) {
+    for (let i: number = 0; i < n;) {
 
         let char: string = word[i]
         let k: number = i + 9;
         let length: number = 0;
-        while (i<n&&i<k&& word[i]===char){
-            length++
-            i++
+        while (i < n && i < k && word[i] === char) {
+            length +=1
+            i +=1
         }
-        comp += length.toString() +char;
+        comp += length.toString() + char;
     }
     return comp;
 
