c80ca1d27d34f6782469577eeac0c0281baa68e8
Shirin <shirin.mohajer@gmail.com>
2024-11-18 12:51:33 -0500

    Time: 0 ms (100%), Space: 51.9 MB (13.7%) - LeetHub

--- 0027-remove-element/0027-remove-element.ts
+++ 0027-remove-element/0027-remove-element.ts
@@ -1,10 +1,10 @@
 function removeElement(nums: number[], val: number): number {
-    let k = 0;
+    let index = 0;
     for (let i = 0; i < nums.length; i++) {
         if (nums[i] !== val) {
-            nums[k] = nums[i]
-            k++
+            nums[index] = nums[i]
+            index++
         }
     }
-    return k
+    return index
 };
