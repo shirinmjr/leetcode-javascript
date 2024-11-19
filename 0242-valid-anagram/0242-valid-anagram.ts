77c8564b2ea73fe067eaa64f3fc53e39b803f21e
Shirin <shirin.mohajer@gmail.com>
2024-11-19 08:02:17 -0500

    Time: 36 ms (8.32%), Space: 57.1 MB (5.04%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.ts
@@ -0,0 +1,13 @@
+function isAnagram(s: string, t: string): boolean {
+    if (s.length !== t.length) return false
+    s = s.split("").sort().join()
+    t = t.split("").sort().join()
+
+    for (let i = 0; i < t.length; i++) {
+        if (s[i] !== t[i]) {
+            return false
+        }
+
+    }
+    return true
+};
