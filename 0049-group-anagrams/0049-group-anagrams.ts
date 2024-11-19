88c8b0dc5aae84a71d532d6544e4a0777d327232
Shirin <shirin.mohajer@gmail.com>
2024-11-19 08:58:45 -0500

    Time: 22 ms (94.34%), Space: 63.7 MB (51.87%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.ts
+++ 0049-group-anagrams/0049-group-anagrams.ts
@@ -3,15 +3,12 @@ function groupAnagrams(strs: string[]): string[][] {
     const myMap = new Map<string, string[]>();
 
     for (const str of strs) {
-        let sortedStr = str.split("").sort().join("")
+        const sortedStr = str.split("").sort().join("")
         if (!myMap.has(sortedStr)) {//create new key and add the first member
             myMap.set(sortedStr, []);
         }
         //find the key and push the member
         myMap.get(sortedStr)!.push(str)
-
-
-
     }
     return Array.from(myMap.values())
 };
