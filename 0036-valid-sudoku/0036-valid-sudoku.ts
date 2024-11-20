392963b669098b47c302688965b6be205a28962b
Shirin <shirin.mohajer@gmail.com>
2024-11-20 15:19:31 -0500

    Time: 5 ms (67.97%), Space: 53.4 MB (70.53%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.ts
+++ 0036-valid-sudoku/0036-valid-sudoku.ts
@@ -2,25 +2,25 @@ function isValidSudoku(board: string[][]): boolean {
     const row: Set<string>[] = Array.from({ length: 9 }, () => new Set())
     const col: Set<string>[] = Array.from({ length: 9 }, () => new Set())
     const box: Set<string>[] = Array.from({ length: 9 }, () => new Set())
-
+    let count = 1
     for (let i = 0; i < 9; i++) {
         for (let j = 0; j < 9; j++) {
             const value = board[i][j]
             if (value === '.') continue
-
-
             const boxIndex = Math.floor(i / 3) * 3 + Math.floor(j / 3)
-
             if (row[i].has(value) || col[j].has(value) || box[boxIndex].has(value)) return false
-
             row[i].add(value)
             col[j].add(value)
             box[boxIndex].add(value)
+
+            // console.log("round", count)
+            // console.log("row===", row)
+            // console.log("col===", col)
+            // console.log("box===", box)
+            //count++
         }
     }
-    // console.log("row===", row)
-    // console.log("col===", col)
-    // console.log("box===", box)
+
     return true
 };
 
