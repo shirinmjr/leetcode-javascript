cf869566c3dfb21a4122050f35b354ede5e3363a
Shirin <shirin.mohajer@gmail.com>
2024-11-20 17:11:13 -0500

    Time: 34 ms (86.89%), Space: 59.4 MB (96.08%) - LeetHub

---
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.ts
@@ -0,0 +1,21 @@
+function longestConsecutive(nums: number[]): number {
+    // console.log(nums)
+    if (nums.length === 0) return 0
+    nums.sort((a, b) => a - b)
+    //console.log(nums)
+    let countArray = []
+    let count = 1
+    for (let i = 1; i <= nums.length; i++) {
+        if ((nums[i - 1] + 1) === nums[i]) {
+            count++
+        } else if (nums[i - 1] === nums[i]) {
+            continue
+        } else {
+            countArray.push(count)
+            count = 1
+        }
+
+    }
+    // console.log(countArray)
+    return Math.max(...countArray)
+};
