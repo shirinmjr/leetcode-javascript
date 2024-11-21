c1275e0f0b62463602e1c39fe30a88d06a0b9176
Shirin <shirin.mohajer@gmail.com>
2024-11-21 11:14:15 -0500

    Time: 59 ms (5.2%), Space: 58 MB (6.61%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.ts
+++ 0022-generate-parentheses/0022-generate-parentheses.ts
@@ -5,8 +5,9 @@ function generateParenthesis(n: number): string[] {
     // Valid IIF open == close == n
     const res: string[] = [];  // To store the result of valid combinations
     const sol: string[] = [];  // To build the current combination of parentheses
-
+let count = 1
     function backtrack(num_open, num_close) {
+       console.log(count)
         // If we have used n open and n close parentheses, add the result
         if (num_open === n && num_close === n) {
             res.push(sol.join(''));  // Join sol array into a string and add to result
@@ -25,6 +26,7 @@ function generateParenthesis(n: number): string[] {
             backtrack(num_open, num_close + 1);  // Recurse with updated counts
             sol.pop();  // Backtrack by removing last character
         }
+ count ++
     }
     // Start the backtracking process
     backtrack(0, 0);
