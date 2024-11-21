a02c27f7f6c9671aed8229450ced95161b72d7d1
Shirin <shirin.mohajer@gmail.com>
2024-11-21 11:11:36 -0500

    Time: 3 ms (45.74%), Space: 52.2 MB (27.2%) - LeetHub

---
+++ 0022-generate-parentheses/0022-generate-parentheses.ts
@@ -0,0 +1,33 @@
+// 1. Brute Force
+function generateParenthesis(n: number): string[] {
+    // Only Add open Paranthesis if open < n
+    // Only add a closing Paranthesis if closed < open
+    // Valid IIF open == close == n
+    const res: string[] = [];  // To store the result of valid combinations
+    const sol: string[] = [];  // To build the current combination of parentheses
+
+    function backtrack(num_open, num_close) {
+        // If we have used n open and n close parentheses, add the result
+        if (num_open === n && num_close === n) {
+            res.push(sol.join(''));  // Join sol array into a string and add to result
+            return;
+        }
+
+        // If we can add an open parenthesis
+        if (num_open < n) {
+            sol.push('(');  // Add open parenthesis to sol
+            backtrack(num_open + 1, num_close);  // Recurse with updated counts
+            sol.pop();  // Backtrack by removing last character
+        }
+
+        if (num_close < num_open) {
+            sol.push(')');  // Add close parenthesis to sol
+            backtrack(num_open, num_close + 1);  // Recurse with updated counts
+            sol.pop();  // Backtrack by removing last character
+        }
+    }
+    // Start the backtracking process
+    backtrack(0, 0);
+
+    return res;
+};
