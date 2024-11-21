13f0b17a90fb03d6d958c9638d3f0e4b4be0702a
Shirin <shirin.mohajer@gmail.com>
2024-11-21 14:29:35 -0500

    Time: 20 ms (70.9%), Space: 73.1 MB (61.82%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.ts
+++ 0739-daily-temperatures/0739-daily-temperatures.ts
@@ -3,10 +3,13 @@ function dailyTemperatures(temperatures: number[]): number[] {
     const stack: number[] = []
 
     for (let i = 0; i < temperatures.length; i++) {
+        // While stack is not empty and
+        // current temperature is greater than the temperature at the top index in the stack
         while (stack.length > 0 && temperatures[stack[stack.length - 1]] < temperatures[i]) {
             const index = stack.pop()!;  // Pop the index of the cooler temperature
             result[index] = i - index;  // The number of days to wait is the difference in indices
         }
+        // Push the current index onto the stack
         stack.push(i)
     }
     return result
