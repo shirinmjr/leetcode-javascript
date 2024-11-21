277ecbe5d95e2d25aea07fdb1fadb8d8c80a6647
Shirin <shirin.mohajer@gmail.com>
2024-11-20 23:03:23 -0500

    Time: 5 ms (71.94%), Space: 52.9 MB (77.48%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.ts
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.ts
@@ -1,14 +1,12 @@
 function evalRPN(tokens: string[]): number {
     const operations = new Set<String>(['+', '-', '*', '/'])
     const stack: number[] = [];
-
     for (const token of tokens) {
         //console.log(stack)
        if (!operations.has(token)) { // if (!isNaN(Number(token))) {
             //if token is a number
             stack.push(Number(token))
         } else {
-
             let a = stack.pop()
             let b = stack.pop()
             // console.log(a,token,b)
@@ -29,16 +27,11 @@ function evalRPN(tokens: string[]): number {
                     break;
                 default:
                     throw new Error(`Invalid operator: ${token}`);
-
             }
             // console.log(result)
             stack.push(result)
         }
-
-
     }
     // The final result will be the only element left in the stack
     return stack.pop()!;
-
-
 };
