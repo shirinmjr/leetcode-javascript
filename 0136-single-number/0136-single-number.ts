3c120eaf26d374d2e4fbb87b3d85dd73099d1796
Shirin <shirin.mohajer@gmail.com>
2024-12-04 12:14:35 -0500

    Time: 15 ms (25.35%), Space: 58.5 MB (5.11%) - LeetHub

--- 0136-single-number/0136-single-number.ts
+++ 0136-single-number/0136-single-number.ts
@@ -1,23 +1,25 @@
-// function singleNumber(nums: number[]): number {
-//     //We need a map
-//     const map = new Map<number, number>()
+function singleNumber(nums: number[]): number {
+    //We need a map
+    const map = new Map<number, number>()
 
-//     for (const num of nums) {
-//         if (map.has(num)) {
-//             map.set(num, map.get(num)! + 1);
-//         } else {
-//             map.set(num, 1)
-//         }
-//     }
-//       for (const [key, value] of map) {
-//         if (value === 1) {
-//             return key;
-//         }
-//     }
+    for (const num of nums) {
+        if (map.has(num)) {
+            map.set(num, map.get(num)! + 1);
+        } else {
+            map.set(num, 1)
+        }
+    }
+    for (const [key, value] of map) {
+        if (value === 1) {
+            return key;
+        }
+    }
 
-//     return -1;
-    
-// };
+    return -1;
+
+};
+/*
 function singleNumber(nums: number[]): number {
     return nums.reduce((prev, val) => prev ^ val, 0)
 };
+*/
