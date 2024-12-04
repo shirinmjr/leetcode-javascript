2815d7516d679b7bb41884cb2a6f0c1e918c210e
Shirin <shirin.mohajer@gmail.com>
2024-12-04 11:57:10 -0500

    Time: 0 ms (100%), Space: 51.4 MB (62.58%) - LeetHub

--- 0812-rotate-string/0812-rotate-string.ts
+++ 0812-rotate-string/0812-rotate-string.ts
@@ -2,3 +2,8 @@ function rotateString(s: string, goal: string): boolean {
     if (s.length !== goal.length) return false; // Lengths must match
     return (s + s).includes(goal); // Check if goal is a substring of s + s
 }
+/*
+Concatenate s with itself (s + s).
+If goal is a substring of this concatenated string, a rotation is possible.
+This covers all possible rotations of s
+*/
