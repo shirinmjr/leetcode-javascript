d245d76caa53c4b5c419764982c8b81ea4a7abc7
Shirin <shirin.mohajer@gmail.com>
2024-12-05 17:44:14 -0500

    Time: 2 ms (66.3%), Space: 53.9 MB (24.27%) - LeetHub

--- 0541-reverse-string-ii/0541-reverse-string-ii.ts
+++ 0541-reverse-string-ii/0541-reverse-string-ii.ts
@@ -1,12 +1,11 @@
 function reverseStr(s: string, k: number): string {
-    const strArray = s.split(""); // Convert string to array for mutability
+    if (s.length === 0) return ""
+    let sArray = s.split("")
 
-    // let tempStr = s.slice(2k)
     for (let i = 0; i < s.length; i += 2 * k) {
-        // Reverse the first k characters of every 2k chunk
-        const end = Math.min(i + k, s.length); // Ensure we don't go out of bounds
-        const reversedPart = strArray.slice(i, end).reverse(); // Reverse the chunk
-        strArray.splice(i, reversedPart.length, ...reversedPart); // Replace original with reversed
+        const end = Math.min(i + k, s.length);
+        const reveresdPart = sArray.slice(i, end).reverse()
+        sArray.splice(i,reveresdPart.length,...reveresdPart)
     }
-    return strArray.join("")
+    return sArray.join("")
 };
