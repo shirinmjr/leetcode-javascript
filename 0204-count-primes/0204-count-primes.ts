5dacde0e5dd936a5a2fa03113724e9f35d5c855e
Shirin <shirin.mohajer@gmail.com>
2024-12-04 23:58:33 -0500

    Time: 1448 ms (7.78%), Space: 114.7 MB (17.5%) - LeetHub

---
+++ 0204-count-primes/0204-count-primes.ts
@@ -0,0 +1,17 @@
+function countPrimes(n: number): number {
+    if (n <= 2) return 0;
+    const isPrime = Array.from({ length: n }, () => true);
+    //const resultArray = Array(n).fill(true);
+    isPrime[0] = false; // 0 is not a prime
+    isPrime[1] = false; // 1 is not a prime
+    console.log(isPrime)
+    let count = n - 2
+    for (let i = 2; i * i < n; i++) {
+        if (isPrime[i]) {
+            for (let j = i * i; j < n; j += i) {
+                isPrime[j] = false; // Mark multiples of i as not prime
+            }
+        }
+    }
+    return isPrime.filter((val) => val).length; // Count true values
+};
