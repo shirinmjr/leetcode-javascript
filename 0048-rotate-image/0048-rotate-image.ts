a030a1591a911755785ce6c9822243c72129a712
Shirin <shirin.mohajer@gmail.com>
2024-12-04 22:26:46 -0500

    Time: 0 ms (100%), Space: 51.6 MB (49.05%) - LeetHub

---
+++ 0048-rotate-image/0048-rotate-image.ts
@@ -0,0 +1,14 @@
+/**
+ Do not return anything, modify matrix in-place instead.
+ */
+function rotate(matrix: number[][]): void {
+    let n = matrix.length;
+    for (let i = 0; i < n; i++) {
+        for (let j = i; j < n; j++) {
+            [matrix[i][j], matrix[j][i]] = [matrix[j][i], matrix[i][j]]
+        }
+    }
+
+    for(let i = 0; i<n;i++)
+    matrix[i].reverse();
+};
