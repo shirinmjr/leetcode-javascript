059c9ef2dfe8a526f4c5045ab1565f43b17e90ab
Shirin <shirin.mohajer@gmail.com>
2024-12-04 23:24:22 -0500

    Time: 0 ms (100%), Space: 51.6 MB (49.05%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.ts
+++ 0048-rotate-image/0048-rotate-image.ts
@@ -3,12 +3,13 @@
  */
 function rotate(matrix: number[][]): void {
     let n = matrix.length;
+    // Step 1: Transpose the matrix
     for (let i = 0; i < n; i++) {
         for (let j = i; j < n; j++) {
             [matrix[i][j], matrix[j][i]] = [matrix[j][i], matrix[i][j]]
         }
     }
-
-    for(let i = 0; i<n;i++)
-    matrix[i].reverse();
+    // Step 2: Reverse each row
+    for (let i = 0; i < n; i++)
+        matrix[i].reverse();
 };
