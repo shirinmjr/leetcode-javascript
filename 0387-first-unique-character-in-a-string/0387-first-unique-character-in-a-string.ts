e7a34c4fbd5212a522e1c0ad261899ff9034bf7e
Shirin <shirin.mohajer@gmail.com>
2024-12-05 19:21:39 -0500

    Time: 56 ms (8.57%), Space: 57.3 MB (25.15%) - LeetHub

---
+++ 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.ts
@@ -0,0 +1,14 @@
+function firstUniqChar(s: string): number {
+
+    const strMap = new Map<string, number>()
+    for (const ch of s) {
+        strMap.has(ch) ? strMap.set(ch, strMap.get(ch) + 1) : strMap.set(ch, 1);
+        // strMap.set(char, (strMap.get(char) || 0) + 1);
+    }
+    console.log(strMap)
+
+    for (let i = 0; i < s.length; i++) {
+        if (strMap.get(s[i]) === 1) return i
+    }
+    return -1
+};
