10793e68677adb61987f61672001feaba4d9c5d9
Shirin <shirin.mohajer@gmail.com>
2024-12-09 12:08:56 -0500

    Time: 0 ms (100%), Space: 51.5 MB (88.92%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.ts
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.ts
@@ -11,24 +11,17 @@
  */
 
 function removeNthFromEnd(head: ListNode | null, n: number): ListNode | null {
-    let slow = head;
     let fast = head;
+    let slow = head;
     let temp = null;
-    // Move fast ahead by n + 1 nodes
     for (let i = 0; i < n; i++) {
-        if (fast === null) return null; // Handle cases where n is larger than the length
         fast = fast.next;
     }
-    // if n is equal to the length of the list it is a shift - case when we need to remove the head
-    // meaning after shifting the fast pinter fast.next is null =>fast is null, while loop not going to run
-    // Move both slow and fast until fast reaches the end
     while (fast !== null) {
-        temp = slow;
-        slow = slow.next;
+        temp = slow
         fast = fast.next;
+        slow = slow.next;
     }
-    // Remove the nth node from the end  
-    temp ? temp.next = slow.next : head = slow.next
-
+    temp ? temp.next= slow.next : head = slow.next
     return head
 };
