5338f4722c46cb290b62e79669ae73925dc02f35
Shirin <shirin.mohajer@gmail.com>
2024-12-09 13:46:46 -0500

    Time: 7 ms (72.73%), Space: 59 MB (87.16%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.ts
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.ts
@@ -1,10 +1,20 @@
 function findDuplicate(nums: number[]): number {
-    const mySet = new Set()
-    for(let i = 0; i < nums.length;i++){
-        if(mySet.has(nums[i])){
-            return nums[i]
-        }else{
-            mySet.add(nums[i])
-        }
+   // Step 1: Use Floyd's Tortoise and Hare to find the intersection point
+    let slow = nums[0];
+    let fast = nums[0];
+
+    do {
+        slow = nums[slow];
+        fast = nums[nums[fast]];
+    } while (slow !== fast);
+
+    // Step 2: Find the entrance to the cycle (duplicate number)
+    slow = nums[0]; // Reset slow to the start
+    while (slow !== fast) {
+        slow = nums[slow];
+        fast = nums[fast];
     }
-};
+
+    return slow; // The duplicate number
+}
+ 
