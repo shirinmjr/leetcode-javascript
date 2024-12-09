0368dc8c4bf1cfcf27f0725b7fc6cda126e1a226
Shirin <shirin.mohajer@gmail.com>
2024-12-09 11:56:01 -0500

    Time: 69 ms (62.39%), Space: 53.5 MB (37.77%) - LeetHub

---
+++ 0237-delete-node-in-a-linked-list/0237-delete-node-in-a-linked-list.ts
@@ -0,0 +1,26 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+
+/**
+ Do not return anything, modify it in-place instead.
+ */
+function deleteNode(node: ListNode | null): void {
+    if (node === null || node.next === null) {
+        return; // Can't delete if node is null or the last node
+    }
+    //need to find the Nth node first
+    node.val = node.next.val
+    node.next = node.next.next
+
+
+
+};
