ecf41ed655e8fb1e1c355f883bef1e7050446300
Shirin <shirin.mohajer@gmail.com>
2024-12-09 17:26:17 -0500

    Time: 3 ms (84.62%), Space: 58.2 MB (24.99%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.ts
+++ 0002-add-two-numbers/0002-add-two-numbers.ts
@@ -11,22 +11,23 @@
  */
 
 function addTwoNumbers(l1: ListNode | null, l2: ListNode | null): ListNode | null {
-    let carry = 0
-    let dummyNode = new ListNode(0)
-    let current = dummyNode
 
-    while (l1 !== null || l2 !== null || carry) {
-        const val1 = l1 ? l1.val : 0
+    let dummyNode = new ListNode(0);
+    let current = dummyNode;
+    let carry = 0
+    //which one is shorter??
+    while (l1 !== null || l2 !== null || carry > 0) {
+        const val1 = l1 ? l1.val : 0 ///if one finishes before the other one we leave 0 instead
         const val2 = l2 ? l2.val : 0
 
-        let sum = (val1 + val2 + carry) 
+        let sum = val1 + val2 + carry
         carry = Math.floor(sum / 10)
 
-        //  current.val = sum
         current.next = new ListNode(sum % 10)
         current = current.next
-        if(l1) l1=l1.next
-        if(l2) l2=l2.next
+
+        if (l1) l1 = l1.next
+        if (l2) l2 = l2.next
     }
     return dummyNode.next
 };
