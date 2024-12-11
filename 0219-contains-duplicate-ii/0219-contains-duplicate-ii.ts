53ffc7c275b66609dcd42905b335531eab4c0292
Shirin <shirin.mohajer@gmail.com>
2024-12-11 12:41:35 -0500

    Time: 17 ms (76.13%), Space: 71.2 MB (31.08%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.ts
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.ts
@@ -3,7 +3,7 @@ function containsNearbyDuplicate(nums: number[], k: number): boolean {
 
     for (let i = 0; i < nums.length; i++) {
         if (numsMap.has(nums[i])) {//first condtion meets
-            const lastIndex = numsMap.get(nums[i]);
+            const lastIndex = numsMap.get(nums[i]); // go find the duplciate and return the index!
             if (i - lastIndex <= k) return true; // Found a duplicate within range k
         }
         // Update the last-seen index for the current number
