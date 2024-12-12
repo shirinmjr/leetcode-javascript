e93f28c007e132fed3e77f1fb19a04469c647dce
Shirin <shirin.mohajer@gmail.com>
2024-12-12 13:41:52 -0500

    Time: 0 ms (100%), Space: 50.8 MB (63.94%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.ts
+++ 0070-climbing-stairs/0070-climbing-stairs.ts
@@ -4,9 +4,9 @@ function climbStairs(n: number): number {
     if (n === 1) return 1;
     if (n === 2) return 2;
 
-if (n in cache) return cache[n];
-
+    if (n in cache) return cache[n];
     return (cache[n] = climbStairs(n - 1) + climbStairs(n - 2));
+   
     /*
         let one = 2;
         let two = 1;
