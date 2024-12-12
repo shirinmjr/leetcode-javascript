3f1f390f5fbac5b5189cd39f02b00d2446d73b68
Shirin <shirin.mohajer@gmail.com>
2024-12-12 13:01:40 -0500

    Time: 0 ms (100%), Space: 50.3 MB (73.92%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.ts
@@ -0,0 +1,29 @@
+function climbStairs(n: number): number {
+
+
+
+    if(n === 1) return 1;
+    if(n === 2) return 2;
+
+    let oneStepBefore = 2;
+    let twoStepsBefore = 1;
+    let allWays = 0;
+
+    // Iterate from the third step to the last step
+    for (let i = 3; i <= n; i++) {
+        allWays = oneStepBefore + twoStepsBefore;  // Current step ways
+        twoStepsBefore = oneStepBefore;  // Move to next step
+        oneStepBefore = allWays;         // Move to next step
+    }
+
+    return allWays;
+    
+
+   /* if(n===1) return 1
+    if (n===2) return 2
+
+    return(climbStairs(n-1) + climbStairs(n-2))
+    */
+
+
+};
