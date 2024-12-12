dded322abae9db363c01a7f0d4387fe7a656adfd
Shirin <shirin.mohajer@gmail.com>
2024-12-12 13:41:16 -0500

    Time: 0 ms (100%), Space: 50.2 MB (77.69%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.ts
+++ 0070-climbing-stairs/0070-climbing-stairs.ts
@@ -1,27 +1,26 @@
+const cache: { [key: number]: number } = {};
 function climbStairs(n: number): number {
-        return n <= 3 
-        ? n 
-        : 2 * climbStairs(n - 2) + climbStairs(n - 3);
+
     if (n === 1) return 1;
     if (n === 2) return 2;
 
-    let one = 2;
-    let two = 1;
-    let temp;
+if (n in cache) return cache[n];
 
-    // Iterate from the third step to the last step
-    for (let i = 3; i <= n; i++) {
-        temp = one; // Current step ways
-        one = one + two // Move to next step
-        two = temp       // Move to next step
-    }
+    return (cache[n] = climbStairs(n - 1) + climbStairs(n - 2));
+    /*
+        let one = 2;
+        let two = 1;
+        let temp;
+    
+        for (let i = 3; i <= n; i++) {
+            temp = one;     // Current step ways
+            one = one + two // Move to next step
+            two = temp       // Move to next step
+        }
+        return one;
+    */
 
-    return one;
-    /* if(n===1) return 1
-     if (n===2) return 2
- 
+    /* Recursive, time Complexity is O(2^n)!
      return(climbStairs(n-1) + climbStairs(n-2))
      */
-
-
 };
