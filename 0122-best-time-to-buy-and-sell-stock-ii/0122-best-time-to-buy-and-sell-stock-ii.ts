414e6837cd7696a763130de6e2b261719104f776
Shirin <shirin.mohajer@gmail.com>
2025-09-18 18:06:59 -0400

    Time: 0 ms (100%), Space: 58.3 MB (8.75%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.ts
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.ts
@@ -2,13 +2,10 @@ function maxProfit(prices: number[]): number {
     let maxProfit: number = 0
     let prevPrice: number = prices[0]
     for (let price of prices) {
-        if (price > prevPrice){
-        maxProfit +=  price - prevPrice
-      
+        if (price > prevPrice) {
+            maxProfit += price - prevPrice
         }
-          prevPrice=price
-}
-
-
+        prevPrice = price
+    }
     return maxProfit
 };
