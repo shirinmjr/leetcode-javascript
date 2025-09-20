d0d40f6a15fd1a4abc6c745fc0bbe3b3e8889c74
Shirin <shirin.mohajer@gmail.com>
2025-09-20 14:49:57 -0400

    Time: 2 ms (74.19%), Space: 64.5 MB (66.69%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.ts
+++ 0053-maximum-subarray/0053-maximum-subarray.ts
@@ -1,9 +1,11 @@
 function maxSubArray(nums: number[]): number {
-    let maxSum = nums[0]
-    let result = nums[0]
+    let maxSum = nums[0];
+    let currentSum = nums[0];
+
     for (let i = 1; i < nums.length; i++) {
-        maxSum = Math.max(nums[i], maxSum + nums[i])
-        result = Math.max(result, maxSum)
+        currentSum = Math.max(nums[i], currentSum + nums[i]);
+        maxSum = Math.max(maxSum, currentSum);
+
     }
-    return result
+    return maxSum;
 };
