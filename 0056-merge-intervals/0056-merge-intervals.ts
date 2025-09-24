74a43205fc93ece1f390c4e3fbe3da5e4ec846fd
Shirin <shirin.mohajer@gmail.com>
2025-09-24 15:43:17 -0400

    Time: 6 ms (95.68%), Space: 64.9 MB (79.16%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.ts
+++ 0056-merge-intervals/0056-merge-intervals.ts
@@ -9,6 +9,7 @@ function merge(intervals: number[][]): number[][] {
     for (let i = 1; i < sorted.length; i++) {
         let curr = res[res.length - 1];
          let last = sorted[i];
+         
         if (last[0] <= curr[1]) {
             // merge: extend the end of curr to cover both intervals
             curr[1] = Math.max(curr[1], last[1]);
