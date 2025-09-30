b27b59b5878778721f0dd591abaca3a130e284d0
Shirin <shirin.mohajer@gmail.com>
2025-09-29 21:41:52 -0400

    Time: 1 ms (21.21%), Space: 56.1 MB (56.13%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.ts
+++ 0088-merge-sorted-array/0088-merge-sorted-array.ts
@@ -6,7 +6,7 @@ function merge(nums1: number[], m: number, nums2: number[], n: number): void {
     for (let i = 0; i < n; i++) {
         nums1[m + i] = nums2[i];
     }
-    nums1.sort((a, b) => a - b)
+    nums1.sort((a, b) => a - b);
 
 
 
