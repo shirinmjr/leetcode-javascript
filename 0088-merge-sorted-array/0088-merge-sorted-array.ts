c08b50979bc374bed733576296b097d8ac761c34
Shirin <shirin.mohajer@gmail.com>
2025-09-29 21:21:15 -0400

    Time: 0 ms (100%), Space: 56.8 MB (13.53%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.ts
+++ 0088-merge-sorted-array/0088-merge-sorted-array.ts
@@ -2,9 +2,10 @@
  Do not return anything, modify nums1 in-place instead.
  */
 function merge(nums1: number[], m: number, nums2: number[], n: number): void {
-    let i = m - 1; // Pointer for the last element in nums1's initial part
-    let j = n - 1; // Pointer for the last element in nums2
-    let k = m + n - 1; // Pointer for the last position in nums1
+    
+let i = m - 1; // Pointer for the last element in nums1's initial part
+let j = n - 1; // Pointer for the last element in nums2
+let k = m + n - 1; // Pointer for the last position in nums1
 
     while (i >= 0 && j >= 0) {
         if (nums1[i] > nums2[j]) {
