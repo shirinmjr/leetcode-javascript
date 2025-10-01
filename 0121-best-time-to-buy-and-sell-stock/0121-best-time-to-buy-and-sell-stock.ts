75c6ee96faf022a49d9e3f69a0325e0ebb5708fc
Shirin <shirin.mohajer@gmail.com>
2025-09-30 20:31:10 -0400

    Time: 1 ms (95.01%), Space: 65.9 MB (59.52%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.ts
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.ts
@@ -1,11 +1,13 @@
 function maxProfit(prices: number[]): number {
     let minPrice = Infinity; // Initialize with a large value
     let maxProfit = 0;
-    for (let price of prices) {
-        // compaire each min to min price - keep pushing to lower it
-        minPrice = Math.min(minPrice, price);
-        // Update maxProfit - keep pushing to get higher and higher profit
-        maxProfit = Math.max(maxProfit, price - minPrice);
+    for (let i = 0; i < prices.length; i++) {
+        if (prices[i] < minPrice) {
+            minPrice = prices[i]
+        }
+        if (prices[i] - minPrice > maxProfit) {
+            maxProfit = prices[i] - minPrice
+        }
     }
     return maxProfit;
 };
