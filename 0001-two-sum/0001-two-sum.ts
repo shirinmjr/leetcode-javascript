1ce9490dde9ea2539db69df185125a1ed4078d75
Shirin <shirin.mohajer@gmail.com>
2025-09-30 22:22:05 -0400

    Time: 3 ms (64.98%), Space: 57.2 MB (40.98%) - LeetHub

--- 0001-two-sum/0001-two-sum.ts
+++ 0001-two-sum/0001-two-sum.ts
@@ -2,7 +2,7 @@ function twoSum(nums: number[], target: number): number[] {
     const myMap = new Map<number, number>();
     for (let i = 0; i < nums.length; i++) {
         let complement = target - nums[i]
-        if (myMap.has(complement)) {
+        if (myMap.has(complement)) {//o(n)
             return [myMap.get(complement), i];
         }
         myMap.set(nums[i], i);
